a766e207d113ff4381a9eaa3f810ef267195606b
Stefan Kehayov <stefankehayov@Stefans-Mac-mini.local>
2021-10-10 21:07:48 +0200

    test

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <h1>Monsters Stk Test</h1>
+        <h1>Monsters Stk Test Test</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={(e) => this.setState({ searchField: e.target.value })}
